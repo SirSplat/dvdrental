8f262cf05a32857bb48e5ca8a2e31a4a0a23a8b3
Leon Rogers <Leon.Rogers@BlueElephantDBA.co.uk>
2024-12-05 11:39:15 +0000

    Revoke public schema privileges

--- db/initdb.sh
+++ db/initdb.sh
@@ -5,5 +5,7 @@ psql -v ON_ERROR_STP=1 --username "dbo" --dbname "postgres" <<-ESQL
     CREATE DATABASE dvdrental TEMPLATE template1;
     COMMENT ON DATABASE dvdrental IS 'Home of dvdrental database. this attempts to use all manner of DDL, DML.';
     ALTER DATABASE dvdrental SET search_path TO rental,exts,sqitch,pgtap;
-    REVOKE ALL ON DATABASE dvdrental FROM public;ß
+    
+    REVOKE ALL ON DATABASE dvdrental FROM public;
+    REVOKE ALL ON SCHEMA public FROM public;
 ESQL
